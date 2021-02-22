c1a72c0d59d72639d05b259890a8d58bc72d2ad1
H. M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-02-22 10:24:35 +0600

    Call back function

--- JS/ScriptPractice3/script1.js
+++ JS/ScriptPractice3/script1.js
@@ -10,3 +10,16 @@ function mySecond() {
     console.log("Goodbye");
 }
 mySecond();
+
+//callbacks2
+function sumDisplayer(sum) {
+    console.log(sum);
+}
+
+function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+let result = myCalculator(5, 5);
+console.log(result);
